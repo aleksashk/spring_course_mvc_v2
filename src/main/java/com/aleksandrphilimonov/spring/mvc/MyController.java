ab12aea2e7d73383229f1b03b1d110e696f9dc2b
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-26 16:08:18 +0300

    @RequestMapping and class

--- src/main/java/com/aleksandrphilimonov/spring/mvc/MyController.java
+++ src/main/java/com/aleksandrphilimonov/spring/mvc/MyController.java
@@ -5,9 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
+@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
@@ -19,22 +18,6 @@ public class MyController {
     public String askEmployeeDetails() {
         return "ask-emp-details-view";
     }
-
-//    @RequestMapping("showDetails")
-//    public String showEmpDetails(){
-//        return "show-emp-details-view";
-//    }
-
-//    @RequestMapping("showDetails")
-//    public String showEmpDetails(HttpServletRequest request, Model model) {
-//
-//        String empName = request.getParameter("employeeName");
-//        empName = "Mr. " + empName;
-//
-//        model.addAttribute("nameAttribute", empName);
-//
-//        return "show-emp-details-view";
-//    }
 
     @RequestMapping("/showDetails")
     public String showDetails(@RequestParam("employeeName") String empName, Model model) {
