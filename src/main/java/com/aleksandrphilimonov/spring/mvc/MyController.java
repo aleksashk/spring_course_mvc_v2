34fd14af9f500b52e76df4d3ad914b4ee1f10415
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-27 13:08:46 +0300

    refactoring add PostMapping to Controller

--- src/main/java/com/aleksandrphilimonov/spring/mvc/MyController.java
+++ src/main/java/com/aleksandrphilimonov/spring/mvc/MyController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,7 +28,7 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
-    @GetMapping(value = "/showDetails")
+    @PostMapping(value = "/showDetails")
     public String showDetails(@Valid @ModelAttribute("employee") Employee emp, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "ask-emp-details-view";
